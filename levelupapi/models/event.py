2c96bd38f670f0bb87eaf770561e3fe1b9e84aa2
mswinkels09 <mswinkels09@gmail.com>
2020-11-04 16:00:51 -0600

    added signup action to views/event.py

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,6 +4,13 @@ from django.db import models
 
 class Event(models.Model):
     """ Event database model"""
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
 
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     day = models.DateField(auto_now=False, auto_now_add=False)
